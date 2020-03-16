70e0bc5a5cdb6e5834b989956292b05fd8fbbcce
Pavel Anni <pavel.anni@gmail.com>
2020-03-15 20:54:52 -0400

    Add README, cd into the script directory

--- crawl.zsh
+++ crawl.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/zsh
 TODAY_DATE=$(date --iso-8601)
 REPORT_NAME=${TODAY_DATE}.html
+mydir=${0:a:h}
+cd $mydir
 wget https://dph.georgia.gov/covid-19-daily-status-report -O ${REPORT_NAME}
 # this is taken from here: https://unix.stackexchange.com/questions/29724/how-to-properly-collect-an-array-of-lines-in-zsh
 # I'm not fluent in Zsh yet, but it works
